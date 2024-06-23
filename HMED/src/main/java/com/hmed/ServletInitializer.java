ee62800f5604bdcd1857cc39a0aca0a3ca526e56
jihaehuh <jihaehuh@naver.com>
2024-06-23 16:19:12 +0900

    xx

--- HMED/src/main/java/com/hmed/ServletInitializer.java
+++ HMED/src/main/java/com/hmed/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.HMED;
+package com.hmed;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
